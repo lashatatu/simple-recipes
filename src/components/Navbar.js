1ace777b65c40ac2c8f28beeed5cd3d40c9611b8
lasha tatulashvili <lashatatu@gmail.com>
2021-04-26 21:47:33 +0400

    24

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,42 @@
 import React from "react"
 import { Link } from "gatsby"
 import { FiAlignJustify } from "react-icons/fi"
+import logo from "../assets/images/logoLasha.svg"
 
 const Navbar = () => {
   return (
-    <div>
-      <FiAlignJustify/>
-      <nav>
-        <ul>
-          <li>
-            <Link to={'/'}>Home</Link>
-          </li>
-          <li>
-            <Link to={'/about'}>About</Link>
-          </li>
-          <li>
-            <Link to={'/contact'}>Contact</Link>
-          </li>
-          <li>
-            <Link to={'/recipes'}>Recipes</Link>
-          </li>
-          <li>
-            <Link to={'/tags'}>Tags</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav className={"navbar"}>
+      <div className={"nav-center"}>
+        <div className={"nav-header"}>
+          <Link to={"/"}>
+            <img src={logo} alt="lashas recipes" />
+          </Link>
+          <button className={"nav-btn"}>
+            <FiAlignJustify />
+          </button>
+        </div>
+        <div className="nav-links show-links">
+          <Link to={"/"} className={"nav-link"} activeClassName={'active-link'}>
+            Home
+          </Link>
+          <Link to={"/recipes"} className={"nav-link"} activeClassName={'active-link'}>
+            Recipes
+          </Link>
+          <Link to={"/tags"} className={"nav-link"} activeClassName={'active-link'}>
+            Tags
+          </Link>
+          <Link to={"/about"} className={"nav-link"} activeClassName={'active-link'}>
+            About
+          </Link>
+          <div className={'nav-link contact-link'}>
+            <Link to={'/contact'} className={'btn'}>
+              Contact
+            </Link>
+
+          </div>
+        </div>
+      </div>
+    </nav>
   )
 }
 
