d42137099c9c5d006a86e3b187e07786fe41f967
lasha tatulashvili <lashatatu@gmail.com>
2021-04-28 03:15:51 +0400

    44

--- src/examples/fetchData.js
+++ src/examples/fetchData.js
@@ -1,39 +1,39 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 
-const ComponentName = () => {
-  const data = useStaticQuery(graphql`
-    {
-      site {
-        siteMetadata {
-          author
-          description
-          simpleData
-          title
-          complexData {
-            age
-            name
-          }
-          person {
-            age
-            name
-          }
+const getData = graphql`
+  {
+    site {
+      siteMetadata {
+        author
+        description
+        simpleData
+        title
+        complexData {
+          age
+          name
+        }
+        person {
+          age
+          name
         }
       }
     }
-  `)
+  }
+`
+const FetchData = () => {
+  const {
+    site: {
+      siteMetadata: { title },
+    },
+  } = useStaticQuery(getData)
   return (
     <div>
-      <h2>
-        {data.site.siteMetadata.person.name}
-      </h2>
-      <div>
-        {data.site.siteMetadata.complexData.map((item,index)=> {
-          return <p key={index}>{item.name}: {item.age}</p>
-        } )}
-      </div>
+      {/*<h2>Name : {data.site.siteMetadata.person.name}</h2>*/}
+
+      <h2>site title is : {title}</h2>
     </div>
   )
 }
 
-export default ComponentName
+export default FetchData
