6eeecfa93715b3afab1096805c55473a877899e0
lasha tatulashvili <lashatatu@gmail.com>
2021-04-28 03:04:19 +0400

    43

---
+++ src/examples/fetchData.js
@@ -0,0 +1,39 @@
+import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+
+const ComponentName = () => {
+  const data = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          author
+          description
+          simpleData
+          title
+          complexData {
+            age
+            name
+          }
+          person {
+            age
+            name
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <div>
+      <h2>
+        {data.site.siteMetadata.person.name}
+      </h2>
+      <div>
+        {data.site.siteMetadata.complexData.map((item,index)=> {
+          return <p key={index}>{item.name}: {item.age}</p>
+        } )}
+      </div>
+    </div>
+  )
+}
+
+export default ComponentName
