5ab22a48796a824343e166c9a9aa6c964bb9f2e4
lasha tatulashvili <lashatatu@gmail.com>
2021-04-27 23:02:42 +0400

    32

--- src/examples/images.js
+++ src/examples/images.js
@@ -10,11 +10,33 @@ const Images = () => {
         <StaticImage
           src={"../assets/images/recipe-1.jpeg"}
           alt={"food"}
-          height={400}
           placeholder="tracedSVG"
           layout="constrained"
           className={"example-img"}
           as={"section"}
+        />
+      </article>
+      <article>
+        <h4>fixed</h4>
+        <StaticImage
+          src={"../assets/images/recipe-1.jpeg"}
+          alt={"food"}
+          placeholder="blurred"
+          layout="fixed"
+          width={200}
+          className={"example-img"}
+          as={"div"}
+        />
+      </article>
+      <article>
+        <h4>Full Width</h4>
+        <StaticImage
+          src={"../assets/images/recipe-1.jpeg"}
+          alt={"food"}
+          placeholder="dominantColor"
+          layout="fullWidth"
+          className={"example-img"}
+          as={"article"}
         />
       </article>
     </Wrapper>
@@ -24,6 +46,10 @@ const Images = () => {
 const Wrapper = styled.section`
   width: 70vw;
   margin: 0 auto;
+  
+  display: grid;
+  text-align: center;
+  gap: 2rem;
 
   article {
     border: 2px solid red;
@@ -31,6 +57,10 @@ const Wrapper = styled.section`
 
   .example-img {
     border-radius: 1rem;
+  }
+  
+  @media (min-width: 992px){
+    grid-template-columns: 1fr 1fr 1fr;
   }
 `
 
