98fcc6e74d1238b383465d1470fef8df5cfa17ea
lasha tatulashvili <lashatatu@gmail.com>
2021-04-28 03:58:29 +0400

    51

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,21 +6,37 @@
 
 module.exports = {
   /* Your site config here */
-  siteMetadata:{
-   title:'Lashas Recipes',
-    description:'Nice and clean recipes site',
-    author:'@lasha',
-    person:{name:'john',age:32},
-    simpleData:['item 1','item 2'],
-    complexData:[
-      {name:'john',age:32},
-      {name:'lasha',age:31},
+  siteMetadata: {
+    title: "Lashas Recipes",
+    description: "Nice and clean recipes site",
+    author: "@lasha",
+    person: {
+      name: "john",
+      age: 32,
+    },
+    simpleData: ["item 1", "item 2"],
+    complexData: [
+      {
+        name: "john",
+        age: 32,
+      },
+      {
+        name: "lasha",
+        age: 31,
+      },
     ],
   },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`
-  ]
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+  ],
 }
