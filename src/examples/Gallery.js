8e11b0096b98a804f4f3ade8ab257def0b8508d8
lasha tatulashvili <lashatatu@gmail.com>
2021-04-29 22:41:46 +0400

    56

--- src/examples/Gallery.js
+++ src/examples/Gallery.js
@@ -1,17 +1,19 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
+import styled from "styled-components"
 
 const query = graphql`
   {
-    allFile(filter: {extension: {ne: "svg"}}) {
+    allFile(filter: { extension: { ne: "svg" } }) {
       nodes {
         name
         childImageSharp {
           gatsbyImageData(
             layout: FIXED
             placeholder: BLURRED
-            transformOptions: {grayscale: true}
             width: 200
+            height: 200
           )
         }
       }
@@ -20,12 +22,35 @@ const query = graphql`
 `
 const Gallery = () => {
   const data = useStaticQuery(query)
-  console.log(data)
+  const nodes = data.allFile.nodes
   return (
-    <div>
-      <h2>simple gallery</h2>
-    </div>
+    <Wrapper>
+      {nodes.map((image, index) => {
+        const { name } = image
+        return (
+          <article key={index} className={'item'}>
+            <GatsbyImage
+              image={image.childImageSharp.gatsbyImageData}
+              alt={name}
+            className={'gallery-img'}/>
+            <p>{name}</p>
+          </article>
+        )
+      })}
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  .item{
+    margin-right: 1rem;
+  }
+  
+  .gallery-img{
+    border-radius: 1rem;
+  }
+`
 
 export default Gallery
